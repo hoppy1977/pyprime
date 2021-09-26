6e5e216cf724b8f391007b3b08b325d6028a048f
Shane Hopcroft <shanehopcroft@hotmail.com>
2021-09-26 20:17:22 +1000

    Update initial implementation - first working version

--- prime.py
+++ prime.py
@@ -1,1 +1,20 @@
-print("Hello world!")
+import math
+
+maxNumber = 10
+
+def is_prime(candidate: int) -> bool:
+    if candidate < 2:
+        return False
+
+    for divisor in range(2, candidate):
+        if(divisor <= math.sqrt(candidate)):
+            if candidate % divisor == 0:
+                return False
+        else:
+            return True
+
+    return True
+
+for candidate in range(1, maxNumber + 1):
+    if is_prime(candidate):
+        print(candidate)
